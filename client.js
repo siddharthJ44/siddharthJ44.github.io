ef8a4f502db8d5f8e460924b41f548703818eec8
siddharthj44 <siddharthjagetia96@gmail.com>
2020-12-28 20:48:24 +0530

    Initial commit

--- client.js
+++ client.js
@@ -9,23 +9,23 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
     return [{
       // we can either provide a button that has a callback function
-      icon: {
+      /*icon: {
         dark: WHITE_ICON,
         light: BLACK_ICON
       },
-      text: 'Callback',
+      text: 'ThoughtFlow MindMap',
       callback: onBtnClick,
       condition: 'edit'
-    }, {
+    }, {*/
       // or we can also have a button that is just a simple url
       // clicking it will open a new tab at the provided url
       icon: {
         dark: WHITE_ICON,
         light: BLACK_ICON
       },
-      text: 'URL',
+      text: 'ThoughtFlow Mindmap',
       condition: 'always',
-      url: 'https://trello.com/inspiration',
+      url: 'https://siddharthj44.github.io/indexnew.html',
       target: 'Inspiring Boards' // optional target for above url
     }];
   }
